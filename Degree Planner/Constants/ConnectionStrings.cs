95c465c7a8640c104d28ce1b8282f80e70c8787a
mdsij <matthewsij1@gmail.com>
2018-12-01 20:08:53 -0600

    added styling

--- Degree Planner/Constants/ConnectionStrings.cs
+++ Degree Planner/Constants/ConnectionStrings.cs
@@ -1,7 +1,7 @@
 ﻿namespace Degree_Planner.Constants {
     public class ConnectionStrings {
 
-        public const string LOCAL = "Server=localhost;Database=Degree_Planner;User=root;";
+        public const string LOCAL = "Server=localhost;Database=Degree_Planner;User=root;password=starwars10";
 	    public const string PRODUCTION = "Server=localhost;Database=Degree_Planner;uid=app;pwd=password";
 	    public const string PRODUCTION_REMOTE = "Server=10.9.2.17;port=3306;Database=Degree_Planner;uid=app;pwd=password";
 		public const string PI = "Server=localhost;Database=Degree_Planner;uid=root;pwd=root";
