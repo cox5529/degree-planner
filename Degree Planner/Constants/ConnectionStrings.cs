b2a228edd5ad3a27520b7a134b2e38cdaafee22e
mdsij <matthewsij1@gmail.com>
2018-12-02 14:10:15 -0600

    finalized terms and conditions. again.

--- Degree Planner/Constants/ConnectionStrings.cs
+++ Degree Planner/Constants/ConnectionStrings.cs
@@ -2,7 +2,7 @@
     public class ConnectionStrings {
 
         public const string LOCAL = "Server=localhost;Database=Degree_Planner;User=root";
-	    public const string PRODUCTION = "Server=localhost;Database=dsegree_planner;uid=app;pwd=password";
+	    public const string PRODUCTION = "Server=localhost;Database=degree_planner;uid=app;pwd=password";
 	    public const string PRODUCTION_REMOTE = "Server=10.9.2.17;port=3306;Database=Degree_Planner;uid=app;pwd=password";
 		public const string PI = "Server=localhost;Database=Degree_Planner;uid=root;pwd=root";
 
